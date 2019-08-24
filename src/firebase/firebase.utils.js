6f67e76ac832bb08bc7520ce8227f925aa8d46ca
Harry Zhang <harryzhang100@gmail.com>
2019-08-23 23:46:42 -0700

    fetched shop data from firestore and stored it inside redux shop reducer

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -48,9 +48,28 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
   });
-  /*fire off the batch request*/
+  
   return await batch.commit();
 };
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+    
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title, 
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+}
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
